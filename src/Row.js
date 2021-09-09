cf355b9cf87c1e7847da59cfcdcec7773ae54999
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-09-09 20:32:27 +0600

    navbar problem fixing going on

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
+  const rowScroll = useRef(null);
   const base_url = "https://image.tmdb.org/t/p/original/";
   useEffect(() => {
     async function fetchData() {
@@ -13,27 +14,65 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const handleRowScroll = (leftOrRight) => {
+    if (leftOrRight === "left") {
+      // rowScroll.current.scrollLeft -= 150;
+
+      rowScroll.current.scrollTo({
+        left: rowScroll.current.scrollLeft - 100,
+        behavior: "smooth",
+      });
+    } else if (leftOrRight === "right") {
+      // rowScroll.current.scrollLeft += 150;
+
+      rowScroll.current.scrollTo({
+        left: rowScroll.current.scrollLeft + 100,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row__posters">
-        {movies.map(
-          (movie) =>
-            ((isLargeRow && movie.poster_path) ||
-              (!isLargeRow && movie.backdrop_path)) && (
-              <div>
-                <img
-                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                  key={movie.id}
-                  src={`${base_url}${
-                    isLargeRow ? movie.poster_path : movie.backdrop_path
-                  }`}
-                  alt={movie.name}
-                ></img>
-                <p className="row__poster__name">{movie.original_name || movie.original_title}</p>
-              </div>
-            )
-        )}
+      <div className="row__posters__container">
+        <div className="row__posters" ref={rowScroll}>
+          {movies.map(
+            (movie) =>
+              ((isLargeRow && movie.poster_path) ||
+                (!isLargeRow && movie.backdrop_path)) && (
+                <div>
+                  <img
+                    className={`row__poster ${
+                      isLargeRow && "row__posterLarge"
+                    }`}
+                    key={movie.id}
+                    src={`${base_url}${
+                      isLargeRow ? movie.poster_path : movie.backdrop_path
+                    }`}
+                    alt={movie.name}
+                  ></img>
+                  <p className="row__poster__name">
+                    {movie.original_name || movie.original_title}
+                  </p>
+                </div>
+              )
+          )}
+        </div>
+        <div className="row__slider__btns">
+          <button
+            onClick={() => handleRowScroll("left")}
+            className="row__slider__right"
+          >
+            Prev
+          </button>
+          <button
+            onClick={() => handleRowScroll("right")}
+            className="row__slider__left"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
