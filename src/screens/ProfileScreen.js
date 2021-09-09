6079c0aa074cb2ea747bfa5213b8ddda3527218b
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-09-09 23:51:43 +0600

    navbar problem fixing going on

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -10,28 +10,28 @@ import "./ProfileScreen.css";
 function ProfileScreen() {
   const user = useSelector(selectUser);
   return (
-    <div className="profileScreen">
-      <Nav />
-      <div className="profileScreen__body">
-        <h1 className="profileScreen__edit__profile">Edit Profile</h1>
-        <div className="profileScreen__info">
-          <img src={userIcon} alt="" />
-          <div className="profileScreen__details">
-            <h2>{user.email}</h2>
-            <div className="profileScreen__plans">
-              <h3>Plans</h3>
-              <PlansScreen />
-              <button
-                onClick={() => auth.signOut()}
-                className="profileScreen__signout"
-              >
-                Sign Out
-              </button>
+        <div className="profileScreen">
+          <Nav />
+          <div className="profileScreen__body">
+            <h1 className="profileScreen__edit__profile">Edit Profile</h1>
+            <div className="profileScreen__info">
+              <img src={userIcon} alt="" />
+              <div className="profileScreen__details">
+                <h2>{user.email}</h2>
+                <div className="profileScreen__plans">
+                  <h3>Plans</h3>
+                  <PlansScreen />
+                  <button
+                    onClick={() => auth.signOut()}
+                    className="profileScreen__signout"
+                  >
+                    Sign Out
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
   );
 }
 
